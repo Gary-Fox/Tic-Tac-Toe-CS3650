eadf751ac2f0249806208533e25df7285bfe0367
Glarbo <garycarrasco@cpp.edu>
2025-08-31 13:06:26 -0800

    Initalizing commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*Created by Gary Carrasco and
+
+ */
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello, World!");
+        System.out.println("This is A tic-tac-toe Project");
+
+
+    }
+}
