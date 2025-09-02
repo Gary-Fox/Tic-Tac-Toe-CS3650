2d76945e7e2aa3362b41a7357362b3903033242f
Gary Fox <67355347+Gary-Fox@users.noreply.github.com>
2025-09-01 23:38:10 -0700

    Add files via upload
    Fleshed out 1 or 2 functions

--- src/main/Board.java
+++ src/main/Board.java
@@ -15,18 +15,54 @@ public class Board {
         return 0;
     }
     
-    public Boolean isFull() {
-        //
-        return false;
+    public Boolean isFull() 
+    {
+        //Establishing Dummy Variable
+        int z = 0;
+
+        //Counting the number of X's and O's in the grid.
+        for (int i = 0; i < rows; i++) 
+        {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == Mark.X) 
+                {
+                    z++;
+                }
+                if (grid[i][j] == Mark.O) 
+                {
+                    z++;
+                }
+            }
+        }
+
+        //If the number of X's and O's is equal to the number of cells
+        //The board is full. Otherwise there's space.
+        if (z == i*j)
+        {
+            return false;
+        }
+        else 
+        {
+            return true;
+        }
     }
     
     public Mark getCell (int r, int c) {
-        //
-        return 0;
+        
+        //Currently prints and returns the required cell.
+        //Not sure if this is what this is for.
+        System.out.println(grid[r][c]);
+        return grid[r][c];
     }
     
-    public void reset() {
-        //
+    public void reset() 
+    {
+        //Filling all the cells with EMPTY mark.
+        for (int i = 0; i < rows; i++) 
+        {
+            Arrays.fill(grid[i], Mark.EMPTY);
+        }
+        
     }
 
     public place(Move mv) {
